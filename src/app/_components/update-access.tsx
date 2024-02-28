cbd8d5eadd9443cdc6d529b8fb31fbb96fcfd2f5
Kiet <kietly019@gmail.com>
2024-02-28 08:56:32 +0700

    update

--- src/app/_components/update-access.tsx
+++ src/app/_components/update-access.tsx
@@ -2,15 +2,18 @@
 
 import { apiClientFetch } from "@/package/api/api-fetch";
 import { formatDate } from "@/package/util";
+import { useEffect } from "react";
 
 export const UpdateAccessServer = ({ accessTime }: { accessTime: string }) => {
   const handleUpdateAccess = async () => {
     const currentDate = new Date();
     if (accessTime !== formatDate(currentDate.toISOString(), "dd/MM/yyyy")) {
       const res = await apiClientFetch("/api/access", {});
-      console.log(res)
+      console.log(res);
     }
   };
-  handleUpdateAccess();
+  useEffect(() => {
+    handleUpdateAccess();
+  }, []);
   return <></>;
 };
